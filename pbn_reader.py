e95d0faec17ddbadddcc5c4397ed09a706ba5c1f
wertqyuio <ultimatenarutofan@gmail.com>
2020-02-24 14:47:18 -0800

    Created pbn parser for deals

--- pbn_reader.py
+++ pbn_reader.py
@@ -1,5 +1,25 @@
-f = open("qt200217.pbn", "r")
+# this file extracts all the deals from the PBN file and the double dummy 
+# tricks
 
-if f.mode == "r":
-    contents = f.read()
-    print(contents)
+# optimizations include skipping file lines during reading
+
+filepath = "qt200217.pbn"
+deals = []
+results = []
+
+with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+        line = fp.readline()
+        if line[:6] == "[Deal ":
+            deal = line[7:line.find("]")-1]
+            deals.append(deal)
+        elif line[:19] == "[DoubleDummyTricks ":
+            result = line[20:line.find("]")-1]
+            results.append(result)
+
+# below to see each deal and result individually
+# for deal in deals:
+#     print(deal)
+# for result in results:
+#     print(result)
